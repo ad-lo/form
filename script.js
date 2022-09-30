3f68f677d4b2a63e83a24f838ab7c91cbcad9966
Chris Sayer <crichardsayer@gmail.com>
2022-09-30 13:52:15 -0400

    version 5

--- script.js
+++ script.js
@@ -5,14 +5,37 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   // using contraint API
   isValid = form.checkValidity();
   // Style main message for error
+ if (!isValid) {
   message.textContent = 'Please fill out all fields.';
   message.style.color = 'red';
   messageContainer.style.borderColor = 'red';
+ } 
+//  check if passwords match
+if (password1EL.value === password2EL.value) {
+  passwordsMatch = true;
+  password1EL.style.borderColor = 'powderblue';
+  password2EL.style.borderColor = 'powderblue';
+} else {
+  passwordsMatch = false;
+  message.textContent = 'Make sure passwords match';
+  message.style.color = 'red';
+  messageContainer.style.borderColor = 'red';
+  password1EL.style.borderColor = 'red';
+  password2EL.style.borderColor = 'red';
+}
+// If form is valid and passwords match 
+if (isValid && passwordsMatch) {
+  message.textContent = 'Successfully Registered!';
+  message.style.color = 'green';
+  messageContainer.style.borderColor = 'green';
+
+}
 }
 
 function processFormData(e) {
